2cc1c727d7e73f4620418e60dddfdbd726e48ce8
nikita.azizov <nik.azizoff2016@gmail.com>
2024-10-29 00:36:43 +0300

    feat: add index.ts files

---
+++ src/controller/index.ts
@@ -0,0 +1,6 @@
+export * from './regController';
+export * from './roomController';
+export * from './winnerController';
+export * from './shipController';
+export * from './gameController';
+export * from './playerController';
